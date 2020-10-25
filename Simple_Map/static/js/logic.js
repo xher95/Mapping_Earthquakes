4f6d3c22da75a029d233d95a2fc69af08db9b084
xher95 <hanabi.xe@gmail.com>
2020-10-25 17:13:33 -0500

    GeoJSON

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,5 +1,5 @@
 // Add console.log to check to see if our code is working.
-console.log("working");
+// console.log("working");
 
 // Create the map object with a center and zoom level.
 // let map = L.map('mapid').setView([40.7, -94.5], 4);
@@ -94,7 +94,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/xher95/Mapping_Earthquakes/master/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/xher95/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {

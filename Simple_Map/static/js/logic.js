67281a0de302985d5ca6a9c0a0e9f8612f888607
xher95 <hanabi.xe@gmail.com>
2020-10-25 14:27:08 -0500

    Adding multiple points

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -10,23 +10,28 @@ let map = L.map("mapid", {
       40.7, -94.5
     ],
     zoom: 4
-  });
+});
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
-// Add a marker to the map for Los Angeles,
-L.circle([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",
-  fillcolor: 'ffffa1'
-}).addTo(map);
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+	console.log(city)
+	L.circleMarker(city.location, {
+    radius: city.population/100000,
+    color: "orange"
+  })
+	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
         accessToken: API_KEY
-    });
+});
     // Then we add our 'graymap' tile layer to the map.
     streets.addTo(map);

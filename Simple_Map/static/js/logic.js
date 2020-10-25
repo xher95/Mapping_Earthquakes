01751eff390b96b468e11dd35b0c8f6d02fbf15d
xher95 <hanabi.xe@gmail.com>
2020-10-25 17:41:53 -0500

    Added style and popup marker

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -34,11 +34,23 @@ let torontoData = "https://raw.githubusercontent.com/xher95/Mapping_Earthquakes/
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/xher95/Mapping_Earthquakes/main/majorAirports.json";
 
+// Create a style for the lines.
+let myStyle = {
+	color: "#ffffa1",
+	weight: 2
+}
+
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " 
+      + feature.properties.dst + "</h3>");
+    }
+  }).addTo(map);
 });
 
 // Then we add our 'graymap' tile layer to the map.
